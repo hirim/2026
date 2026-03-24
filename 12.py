16fe9c643a1408da806c6535560b149f567d6019
hirim <53498120+hirim@users.noreply.github.com>
2026-03-24 22:37:27 +0900

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,38 +1,31 @@
-import RPi.GPIO as GPIO
+from gpiozero import Button
 import time
-
 from picamera2 import Picamera2, Preview
 
-swPin = 14
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(swPin,GPIO.IN,pull_up_down = GPIO.PUD_DOWN)
+button = Button(2)
 
 picam2 = Picamera2()
-picam2.preview_configuration.main.size = (800,600)
-picam2.configure('preview')
-picam2.start(show_preview = True)
-
-oldSw = 0
-newSw = 0
+picam2.preview_configuration.main.size = (800, 600)
+picam2.configure("preview")
+picam2.start(show_preview=True)
 
 cnt = 1
 
 try:
     while True:
-        newSw = GPIO.input(swPin)
-        if newSw != oldSw:
-            oldSw = newSw
+        if button.is_pressed:
 
-            if newSw == 1:
-                file_name = 'click ' + str(cnt)
-                print(file_name)
-                picam2.capture_file(file_name+'.jpg')
-                cnt += 1
-            time.sleep(0.2)
+            file_name = "Click" + str(cnt)
+            print(file_name + ".jpg")
+
+            picam2.capture_file(file_name + ".jpg")
+
+            cnt += 1
+
+            while button.is_pressed:
+                time.sleep(0.01)
+
+        time.sleep(0.01)
 
 except KeyboardInterrupt:
     pass
-
-GPIO.cleanup()
